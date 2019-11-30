3015fac7309934d3bb0568c46ea9f14571ac4c04
jpranay08 <54579190+jpranay08@users.noreply.github.com>
2019-11-30 15:09:39 +0530

    Update kubernetes-deploy.bat

--- PyMicroservices-master/Source/kubernetes-deploy.bat
+++ PyMicroservices-master/Source/kubernetes-deploy.bat
@@ -1,4 +1,4 @@
-SET PATH=%PATH%;C:\Users\Pranay Chowdary\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
+SET PATH=%PATH%;C:\Users\PranayChowdary\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
 SET IMAGE_NAME=gcr.io/prabhacloud/gpymicro:%BuildNumber%
-cd C:\Users\Pranay Chowdary\Desktop\PyMicroservices-master\Source  
+cd C:\Users\PranayChowdary\Desktop\PyMicroservices-master\Source  
 kubectl set image deployment/py-gmicro py-gmicro=%IMAGE_NAME%
